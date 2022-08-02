4f408b71ef7b79a5be966f30e50c6226e2003e34
Balikistechie <109273345+Balikistechie@users.noreply.github.com>
2022-08-02 09:10:02 +0100

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,8 @@
+let email=document.getElementById('Email');
+let Password=document.getElementById('password');
+
+function  myChange(){
+  var btn=document.getElementById('btn')
+  email.innerHTML='Phone Number';
+  document.body.style.backgroundColor='#dff30b';
+}
